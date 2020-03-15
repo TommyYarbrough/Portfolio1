e3df7e066347766d143c0a1bdfaf2367b0e0637f
TommyYarbrough <tommhockey20@gmail.com>
2020-03-15 13:59:38 -0700

    update

---
+++ extentions/jsLearningData/learningDataDeclaring-mathematics.js
@@ -0,0 +1,13 @@
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /=12;
+b /=4;
+c /=11;
+a += 1;
+b -= 2;
+c *= 8;
+// These statements will reduce time by using augmented variables instead of var myName = myName + 2.
+
