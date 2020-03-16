16799f321d9fb414a2e4572ca3031db3dbe46922
TommyYarbrough <tommhockey20@gmail.com>
2020-03-15 18:44:34 -0700

    updating PortFolio
    adding new features to vusual studio code

---
+++ Projects/newLoops.js
@@ -0,0 +1,8 @@
+let myArray = []; // initial number placed doesnt change the array just adds the number before the array.
+let i = 0; // when swicthed from 0 to 1 you start with the number you entered.
+while(i < 15) { // ending of the number placed in () will be -1 for the ending number.
+  myArray.push(i); //moves to the next element in the array. 
+  i++; // adding 1.
+}
+
+console.log(myArray)
