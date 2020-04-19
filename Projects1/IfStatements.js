1b6bc750a62b32b43fa8c320e3c627292875b788
TommyYarbrough <tommhockey20@gmail.com>
2020-04-19 10:10:56 -0700

    function that updates the collection with your input of an id,property,value

--- Projects1/IfStatements.js
+++ Projects1/IfStatements.js
@@ -42,4 +42,43 @@ var myDog = {
     "tails": 1,
     "friends": ["dogs", "Me"]
   };
-  
+
+
+  var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+function updateRecords(id, prop, value) {
+  if (value === "") {
+    delete collection[id][prop];
+  } else if (prop === "tracks") {
+    collection[id][prop] = collection[id][prop] || [];
+    collection[id][prop].push(value);
+  } else {
+    collection[id][prop] = value;
+  } 
+
+  return collection;
+}
+updateRecords(id, prop, value) // this is a function to update a record album 
